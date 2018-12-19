e5e376500626fb42bac1e35541edee62256013db
Pierrick Prudhomme <pierrick.prudhomme@environnement-recycling.com>
2018-12-19 08:23:05 +0100

    feat(slider): update documentation

--- server/files/javascript/slider.js
+++ server/files/javascript/slider.js
@@ -1,29 +1,85 @@
-semantic.dropdown = {};
+semantic.slider = {};
 
 // ready event
-semantic.dropdown.ready = function() {
-
+semantic.slider.ready = function() {
   // selector cache
   var
-    $slider       = $('.example').find('.ui.slider'),
-    // alias
-    handler
+    $slider     = $('.ui.slider')
   ;
-
-  // event handlers
-  handler = {
-
-  };
-
   $slider
-    .slider()
+    .slider({
+      min: 0,
+      max: 10,
+      start: 5
+    })
+  ;
+  
+  $('#slider-1')
+    .slider({
+      min: 0,
+      max: 10,
+      start: 5,
+      onChange: function(value) {
+        $('#slider-input-1').val(value)
+      }
+    })
   ;
 
+  $('#slider-range')
+    .slider({
+      min: 5,
+      max: 100,
+      start: 10,
+      end: 90,
+      step: 5,
+      debug: true,
+      verbose: true,
+      onChange: function(range, firstVal, secondVal) {
+        if (firstVal > secondVal) {
+          $('#range-slider-input-1').val('|' + firstVal + " - " + secondVal + '| = ' + range);
+        } else {
+          $('#range-slider-input-1').val('|' + secondVal + " - " + firstVal + '| = ' + range);
+        }
+      }
+    })
+  ;
 
+  $('#slider-custom-step')
+    .slider({
+      min: 0,
+      max: 10,
+      start: 4,
+      step: 2,
+      onChange: function(value) {
+        $('#slider-input-3').val(value)
+      }
+    })
+  ;
+
+  $('#slider-unstepped')
+    .slider({
+      min: 0,
+      max: 10,
+      start: 3.82,
+      step: 0,
+      onChange: function(value) {
+        $('#slider-input-unstepped').val(value)
+      }
+    })
+  ;
+
+  $('#slider-smooth')
+    .slider({
+      min: 0,
+      max: 10,
+      start: 4,
+      smooth: true
+    })
+  ;
 };
 
 
 // attach ready event
 $(document)
-  .ready(semantic.dropdown.ready)
+  .ready(semantic.slider.ready)
 ;
